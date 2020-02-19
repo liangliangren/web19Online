2eb042fb6da50ca4bc2216c71d5f2cea7ca019f3
liangliangren <373289962@qq.com>
2020-02-19 11:29:50 +0800

    20200219上午

---
+++ 在线课程代码笔记/NodeJS/day05/cookie.js
@@ -0,0 +1,49 @@
+//cookie-parser
+//注意事项：cookie在不同的域名下面不能相互访问
+
+//在本地配置一个域名 
+//abc.com       顶级域名
+//news.abc.com  二级域名
+//2020.hnchennuo.com  二级域名
+//mp.weixin.qq.com
+//C:\Windows\System32\drivers\etc找到hosts 用记事本打开  在最底部添加  127.0.0.1 news.abc.com
+
+
+//先下载   cnpm install cookie-parser --save
+//		 cnpm install express --save
+let express=require("express");
+
+//引入
+let cookieParser=require("cookie-parser");
+
+let app=new express();  //new关键字可以省掉
+
+//使用
+app.use(cookieParser())
+
+//设置cookie的路由
+app.get("/setcookie",(req,res)=>{
+	//第一个参数:名称
+	//第二个参数：cookie的值
+	//第三个参数：cookie的配置信息
+	res.cookie("username","llr123",{
+		maxAge:24*60*60*1000,    //maxAge配置cookie的过期时间
+		domain:".abc.com"
+	})
+	
+	res.send("cookie设置成功")
+})
+
+//获取cookie
+app.get("/getcookie",(req,res)=>{
+	console.log(req.cookies)  //{ username: 'llr' }
+	res.send("cookie获取成功：欢迎"+req.cookies.username+"进入网站！！")
+})
+
+
+//
+
+
+app.listen(3000,function(){
+	console.log("3000running")
+})
