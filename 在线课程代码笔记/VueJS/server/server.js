95b504c6378db4bea1fec81e549dff66e357bb2c
liangliangren <373289962@qq.com>
2020-03-02 11:35:11 +0800

    20200302上午

---
+++ 在线课程代码笔记/VueJS/server/server.js
@@ -0,0 +1,18 @@
+//查询语句 SELECT user_name,user_age FROM `user` WHERE user_age >= 18 AND user_age < 30
+//插入语句 INSERT INTO `user` (user_name) VALUES ("liang")
+//插入语句 INSERT INTO `user` (user_name,login_password,user_phone,user_age) VALUES ("靓靓人","123456","13673618137","28")
+//插入语句 INSERT INTO 表名 (字段1,字段2,...) VALUES (值1,值2,...)
+
+//更新数据 UPDATE `user` SET user_name="liangliang" WHERE user_name="liang"
+//更新数据 UPDATE `user` SET user_name="abc",login_password="654321",user_phone="12345678900",user_age="10" WHERE user_id="7"
+//更新数据 UPDATE 表名 SET 字段1=值1,字段2=值2,... WHERE 条件表达式
+
+//删除数据  DELETE FROM `user` WHERE user_id="7"  删除满足条件的数据
+
+//删除数据  DELETE FROM `user`   删除user表中所有的数据
+
+
+//新建一个数据库名称为 shop
+
+//在shop数据库中新建user 表     user_id, user_name,login_password,user_phone,user_sex,user_age
+
